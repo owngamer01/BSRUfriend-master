5b493d6f91e47fa7eb4e7775e63101417958ce3c
DEVMRFAT <echolaojue@gmail.com>
2017-02-17 16:21:02 +0700

    now 17feb

--- app/src/main/java/appewtc/masterung/bsrufriend/ListFriend.java
+++ app/src/main/java/appewtc/masterung/bsrufriend/ListFriend.java
@@ -1,8 +1,11 @@
 package appewtc.masterung.bsrufriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import org.json.JSONArray;
@@ -26,10 +29,10 @@ public class ListFriend extends AppCompatActivity {
             String strJSON = getUser.get();
 
             JSONArray jsonArray = new JSONArray(strJSON);
-            String[] nameStrings = new String[jsonArray.length()];
-            String[] imageStrings1 = new String[jsonArray.length()];
-            String[] latStrings1 = new String[jsonArray.length()];
-            String[] lngStrings1 = new String[jsonArray.length()];
+            final String[] nameStrings = new String[jsonArray.length()];
+            final String[] imageStrings1 = new String[jsonArray.length()];
+            final String[] latStrings1 = new String[jsonArray.length()];
+            final String[] lngStrings1 = new String[jsonArray.length()];
 
             for (int i = 0; i < jsonArray.length(); i++) {
 
@@ -43,6 +46,22 @@ public class ListFriend extends AppCompatActivity {
 
             FriendAdapter friendAdapter = new FriendAdapter(ListFriend.this, imageStrings1, nameStrings);
             listView.setAdapter(friendAdapter);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Intent intent = new Intent(ListFriend.this, DetailFriend.class);
+                    intent.putExtra("Name", nameStrings[i]);
+                    intent.putExtra("Image", imageStrings1[i]);
+                    intent.putExtra("Lat", latStrings1[i]);
+                    intent.putExtra("Lng", lngStrings1[i]);
+                    startActivity(intent);
+
+                }
+            });
+
+
+
 
 
 
